132e10a62bafadd1211ea62cdc29a602578be033
Strahinja <68552692+Sluki03@users.noreply.github.com>
2026-03-13 20:54:04 +0100

    Updated default favorites
    - and tiny css update

--- data.js
+++ data.js
@@ -5,29 +5,9 @@ export const fList = [
         link: "https://youtube.com/"
     },
     {
-        title: "gmail personal",
-        image: "https://www.google.com/s2/favicons?domain=https://mail.google.com&sz=64",
-        link: "https://mail.google.com/mail/u/1/#inbox"
-    },
-    {
-        title: "gmail",
-        image: "https://www.google.com/s2/favicons?domain=https://mail.google.com&sz=64",
-        link: "https://mail.google.com/mail/u/0/#inbox"
-    },
-    {
-        title: "translate",
-        image: "https://www.google.com/s2/favicons?domain=https://translate.google.com&sz=64",
-        link: "https://translate.google.rs/u/1/?hl=sr&sl=en&tl=de&op=translate"
-    },
-    {
-        title: "eponuda",
-        image: "https://www.google.com/s2/favicons?domain=https://www.eponuda.com&sz=64",
-        link: "https://www.eponuda.com/"
-    },
-    {
-        title: "kupujem prodajem",
-        image: "https://www.google.com/s2/favicons?domain=https://novi.kupujemprodajem.com&sz=64",
-        link: "https://novi.kupujemprodajem.com/"
+        title: "github",
+        image: "https://www.google.com/s2/favicons?domain=https://github.com&sz=64",
+        link: "https://github.com"
     }
 ];
 
